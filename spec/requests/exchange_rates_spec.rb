61fd981724dd9cc1e034fa47713801584c498023
maxim <axmaxon@gmail.com>
2023-02-08 12:44:46 +0500

    Add admin-page prototype

--- spec/requests/exchange_rates_spec.rb
+++ spec/requests/exchange_rates_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "ExchangeRates", type: :request do
-  describe "GET /" do
-    it "returns http success" do
-      get "/"
+RSpec.describe 'ExchangeRates', type: :request do
+  describe 'GET /' do
+    it 'returns http success' do
+      get '/'
       expect(response).to have_http_status(:success)
     end
   end
